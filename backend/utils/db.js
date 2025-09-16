4c86559bcd406c610509ec3bd1ec12d32b39ede0
tanishq1172005 <tanishq1172005@gmail.com>
2025-09-16 11:20:50 +0530

    new commit

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = ()=>{
     try{
-        mongoose.connect(process.env.MONGODB_URI)
+        mongoose.connect(process.env.MONGODB_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
+        tlsAllowInvalidCertificates: false,
+})
         console.log("connected")
     }catch(err){
         console.error(err)
